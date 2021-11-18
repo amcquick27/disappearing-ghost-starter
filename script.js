61cc768fb62c4c9230f7b36cf8a3404fa881c379
Glitch (snap-ghost) <none>
2021-11-18 01:44:26 +0000

    Finished all the steps.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+$(".hide-button").click(function() {
+  $(".ghost-image").hide();
+});
+$(".show-button").click(function() {
+  $(".ghost-image").show();
+});
+$(".update-img-button").click(function() {
+  $(".ghost-image").css("width" ,"50px");
+});
 
+$(".message-button").click(function() {
+  let userMessage = $(".input").val();
+  $(".message").append("Look what you wrote: "+ userMessage);
+});
+$(".name-button").click(function() {
+  $("h1").text("My name is Jeff");
+});
